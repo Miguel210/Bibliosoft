8e7daeddc5d56f801dc0d78b624ba007db049360
BrayanGixR6S <brayanfierrelix@gmail.com>
2024-03-05 12:57:05 -0600

    Conexion bd 05/03/2024

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, flash, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.secret_key = 'mysecretkey'
+
+app.secret_key = 'mysecretkey'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '12345678'
+app.config['MYSQL_DB'] = 'biblioteca'
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
