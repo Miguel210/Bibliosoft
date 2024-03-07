9a268733b10679f1c610b14237dc7299e88de784
BrayanGixR6S <brayanfierrelix@gmail.com>
2024-03-07 12:38:03 -0600

    conexion a la bd 07-03-2024

--- app.py
+++ app.py
@@ -9,12 +9,20 @@ app.secret_key = 'mysecretkey'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '12345678'
-app.config['MYSQL_DB'] = 'biblioteca'
+app.config['MYSQL_DB'] = 'bibliosoft'
 mysql = MySQL(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/perfil')
+def inicio():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM perfil')
+    data = cur.fetchall()
+    cur.close()
+    return render_template('perfil.html', perfiles=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
